5679e79e31d2a047ddae9aeb77425bccf844ed76
Puneeth0941 <puneethgowdag20@gmail.com>
2026-07-26 22:55:43 +0530

    Use authenticated API instance for farm service

--- frontend/src/services/farmService.ts
+++ frontend/src/services/farmService.ts
@@ -1,6 +1,4 @@
-import axios from "axios";
-
-const API = import.meta.env.VITE_API_URL + "/farm";
+import { API } from "./authService";
 
 export interface Farm {
   id?: number;
@@ -12,22 +10,28 @@ export interface Farm {
   coconut_trees: number;
 }
 
+const BASE = "/farm";
+
+// Get all farms
 export const getAllFarms = async () => {
-  const response = await axios.get(`${API}/all`);
+  const response = await API.get(`${BASE}/all`);
   return response.data;
 };
 
+// Add farm
 export const addFarm = async (farm: Farm) => {
-  const response = await axios.post(`${API}/add`, farm);
+  const response = await API.post(`${BASE}/add`, farm);
   return response.data;
 };
 
+// Update farm
 export const updateFarm = async (id: number, farm: Farm) => {
-  const response = await axios.put(`${API}/update/${id}`, farm);
+  const response = await API.put(`${BASE}/update/${id}`, farm);
   return response.data;
 };
 
+// Delete farm
 export const deleteFarm = async (id: number) => {
-  const response = await axios.delete(`${API}/delete/${id}`);
+  const response = await API.delete(`${BASE}/delete/${id}`);
   return response.data;
 };
